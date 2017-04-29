7843190a4bb1eef72345caaf3d066eb7b71dfb25
Joseph Dion <joejdion@gmail.com>
2017-04-28 23:28:44 -0700

    Implemented achievement details page, and pomodoro style timer
    Added achievement details page. This allows users to see a hi res display of their achievement and read its description.
    
    Added extra achievements
    
    Added pomodoro style timer and implemented it inside both the naming and timing activities.
    
    TODO: You need to move all sql realted stuff to an ASync task to reduce the massive lag you get with gridview.

---
+++ app/src/main/java/com/example/joe/stopwatch/activities/acheivementDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.joe.stopwatch.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.joe.stopwatch.R;
+
+public class acheivementDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acheivement_details);
+    }
+}
---
+++ app/src/main/java/com/example/joe/stopwatch/util/PomodoroTimer.java
@@ -0,0 +1,8 @@
+package com.example.joe.stopwatch.util;
+
+/**
+ * Created by Joe on 4/28/17.
+ */
+
+public class PomodoroTimer {
+}
